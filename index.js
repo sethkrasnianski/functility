25418f93e411df09c20968d776725a535075af77
Seth Krasnianski <seth@mojotech.com>
2015-02-10 23:11:08 -0500

    require_all: Fix file filter check

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var require_all = function(location) {
   fs
     .readdirSync(location)
     .filter(function(file) {
-      return (file.indexOf(".") !== 0) && (file !== "index.");
+      return (file.indexOf(".") !== 0) && (file.indexOf("index.") == -1);
     })
     .forEach(function(file) {
       var module = file.substr(0, file.indexOf('.js'));

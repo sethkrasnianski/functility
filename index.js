2484e8cbdf3871d45cd641551a2e35fce7062824
Seth Krasnianski <seth@mojotech.com>
2015-02-10 23:01:43 -0500

    Ambiguous file filtering

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var require_all = function(location) {
   fs
     .readdirSync(location)
     .filter(function(file) {
-      return (file.indexOf(".") !== 0) && (file !== "index.js");
+      return (file.indexOf(".") !== 0) && (file !== "index.");
     })
     .forEach(function(file) {
       var module = file.substr(0, file.indexOf('.js'));

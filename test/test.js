ae9681fcfe580d58bd66bbb664407030187257a1
Seth Krasnianski <seth@mojotech.com>
2015-02-13 22:46:01 -0500

    test: Move cipher to variable

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ var assert     = require("assert")
   , functility = require('../')
   , env        = process.env;
 
+var cipher = "4e7a3c6c2e833daea4a3fd3ccd0ab361$3ac83e71b8ae8d9929e6b2ab873112e0$a5d2171e8bb0dde029dfabf3ed5109bd7be7e1d11c693d094f4faac19d3a491f";
+
 describe('require_all', function() {
   it('should return an object with 2 properties', function(done) {
     var module = functility.require_all(__dirname + "/examples");
@@ -15,13 +17,13 @@ describe('crypt', function() {
     var encrypt = functility.crypt.encrypt;
     var cipher  = encrypt('testingthisout');
 
-    assert("4e7a3c6c2e833daea4a3fd3ccd0ab361$3ac83e71b8ae8d9929e6b2ab873112e0$a5d2171e8bb0dde029dfabf3ed5109bd7be7e1d11c693d094f4faac19d3a491f", cipher);
+    assert(cipher, cipher);
     done();
   });
 
   it('should decrypt a cipher', function(done) {
     var decrypt = functility.crypt.decrypt;
-    var text    = decrypt("4e7a3c6c2e833daea4a3fd3ccd0ab361$3ac83e71b8ae8d9929e6b2ab873112e0$a5d2171e8bb0dde029dfabf3ed5109bd7be7e1d11c693d094f4faac19d3a491f");
+    var text    = decrypt(cipher);
 
     assert("testringthisout", text);
     done();

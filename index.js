a83d8de03001bcae3c3f3eea6f5efb227986071f
Seth Krasnianski <sethkrasnianski@gmail.com>
2019-01-11 01:03:54 -0500

    Dynamically build path to caller's root directory with requireAll

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const requireAll = require('./lib/require_all');
 
 module.exports = {
   requireAll: requireAll,
-  crypto: requireAll(__dirname + '/lib/crypto')
+  crypto: requireAll('./lib/crypto')
 };
